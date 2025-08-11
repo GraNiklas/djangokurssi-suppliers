27c5fff34ae388adf4da9d1e7c890423ea974505
Niklas Granqvist <niklasgranqvist@windowslive.com>
2025-08-11 15:31:41 +0300

    toinen commit, productpage ja supplierpage toimii listaus ja lisäys

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import landingview, productlistview, supplierlistview
+from .views import addproduct, landingview, productlistview, supplierlistview, addsupplier
 
 urlpatterns= [
 	path('',landingview),
-	path('suppliers/',supplierlistview),
+
 	path('products/',productlistview),
+	path('add-product/',addproduct),
+
+	path('suppliers/',supplierlistview),
+	path('add-supplier/',addsupplier),
+
+	
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Supplier, Product
 
 def landingview(request):
     return render(request, 'landingpage.html')
 
 def productlistview(request):
-    return render(request, 'productlist.html')
+    productlist = Product.objects.all()
+    supplierlist = Supplier.objects.all()
+    context = {'products': productlist ,'suppliers':supplierlist}
+    return render(request, 'productlist.html',context)
+
+def addproduct(request):
+    a = request.POST['productname']
+    b = request.POST['price']
+    c = request.POST['stock']
+    d = request.POST['supplier']
+    Product(productname = a, price = b, stock = c, supplier = Supplier.objects.get(id = d)).save()
+    return redirect(request.META['HTTP_REFERER'])
 
 def supplierlistview(request):
-    teststring = "this is test string"
-    context = {'x':teststring}
+    supplierlist = Supplier.objects.all()
+    context = {'suppliers': supplierlist}
     return render(request, 'supplierlist.html',context)
+
+def addsupplier(request):
+    a = request.POST['companyname']
+    b = request.POST['contactname']
+    c = request.POST['address']
+    d = request.POST['phone']
+    e = request.POST['email']
+    f = request.POST['country']
+    Supplier(companyname = a, contactname = b, address = c,phone = d, email = e, country = f).save()
+    return redirect(request.META['HTTP_REFERER'])
